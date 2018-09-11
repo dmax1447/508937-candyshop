90760c92df756406073ce9aad71538b75796882d
Max Dragaytsev <dmax1447@gmail.com>
2018-09-11 10:48:03 +0300

    внес правки по замечениям

--- js/goods.js
+++ js/goods.js
@@ -1,8 +1,9 @@
 'use strict';
-var mokNames = ['Чесночные сливки', 'Огуречный педант', 'Молочная хрюша', 'Грибной шейк', 'Баклажановое безумие', 'Паприколу итальяно', 'Нинзя-удар васаби', 'Хитрый баклажан', 'Горчичный вызов', 'Кедровая липучка', 'Корманный портвейн', 'Чилийский задира', 'Беконовый взрыв', 'Арахис vs виноград', 'Сельдерейная душа', 'Початок в бутылке', 'Чернющий мистер чеснок', 'Раша федераша', 'Кислая мина', 'Кукурузное утро', 'Икорный фуршет', 'Новогоднее настроение', 'С пивком потянет', 'Мисс креветка', 'Бесконечный взрыв', 'Невинные винные', 'Бельгийское пенное', 'Острый язычок'];
-var mokPictures = ['gum-cedar.jpg', 'gum-chile.jpg', 'gum-eggplant.jpg', 'gum-mustard.jpg', 'gum-portwine.jpg', 'gum-wasabi.jpg', 'ice-cucumber.jpg', 'ice-eggplant.jpg', 'ice-garlic.jpg', 'ice-italian.jpg', 'ice-mushroom.jpg', 'ice-pig.jpg', 'marmalade-beer.jpg', 'marmalade-caviar.jpg', 'marmalade-corn.jpg', 'marmalade-new-year.jpg', 'marmalade-sour.jpg', 'marshmallow-bacon.jpg', 'marshmallow-beer.jpg', 'marshmallow-shrimp.jpg', 'marshmallow-spicy.jpg', 'marshmallow-wine.jpg', 'soda-bacon.jpg', 'soda-celery.jpg', 'soda-cob.jpg', 'soda-garlic.jpg', 'soda-peanut-grapes.jpg',
+var mockNames = ['Чесночные сливки', 'Огуречный педант', 'Молочная хрюша', 'Грибной шейк', 'Баклажановое безумие', 'Паприколу итальяно', 'Нинзя-удар васаби', 'Хитрый баклажан', 'Горчичный вызов', 'Кедровая липучка', 'Корманный портвейн', 'Чилийский задира', 'Беконовый взрыв', 'Арахис vs виноград', 'Сельдерейная душа', 'Початок в бутылке', 'Чернющий мистер чеснок', 'Раша федераша', 'Кислая мина', 'Кукурузное утро', 'Икорный фуршет', 'Новогоднее настроение', 'С пивком потянет', 'Мисс креветка', 'Бесконечный взрыв', 'Невинные винные', 'Бельгийское пенное', 'Острый язычок'];
+var mockPictures = ['gum-cedar.jpg', 'gum-chile.jpg', 'gum-eggplant.jpg', 'gum-mustard.jpg', 'gum-portwine.jpg', 'gum-wasabi.jpg', 'ice-cucumber.jpg', 'ice-eggplant.jpg', 'ice-garlic.jpg', 'ice-italian.jpg', 'ice-mushroom.jpg', 'ice-pig.jpg', 'marmalade-beer.jpg', 'marmalade-caviar.jpg', 'marmalade-corn.jpg', 'marmalade-new-year.jpg', 'marmalade-sour.jpg', 'marshmallow-bacon.jpg', 'marshmallow-beer.jpg', 'marshmallow-shrimp.jpg', 'marshmallow-spicy.jpg', 'marshmallow-wine.jpg', 'soda-bacon.jpg', 'soda-celery.jpg', 'soda-cob.jpg', 'soda-garlic.jpg', 'soda-peanut-grapes.jpg',
   'soda-russian.jpg'];
-var mokContents = ['молоко', 'сливки', 'вода', 'пищевой краситель', 'патока', 'ароматизатор екона', 'ароматизатор свинца', 'ароматизатор дуба, идентичный натуральному', 'ароматизатор артофеля', 'лимонная кислота', 'загуститель', 'эмульгатор', 'консервант: сорбат калия', 'посолочная смесь: соль, нитрит натрия', 'ксилит', 'карбамид', 'вилларибо', 'виллабаджо'];
+var MockContents = ['молоко', 'сливки', 'вода', 'пищевой краситель', 'патока', 'ароматизатор бекона', 'ароматизатор свинца', 'ароматизатор дуба, идентичный натуральному', 'ароматизатор картофеля', 'лимонная кислота', 'загуститель', 'эмульгатор', 'консервант: сорбат калия', 'посолочная смесь: соль, нитрит натрия', 'ксилит', 'карбамид', 'вилларибо', 'виллабаджо'];
+var starsRatingStyles = ['stars__rating--one', 'stars__rating--two', 'stars__rating--three', 'stars__rating--four', 'stars__rating--five'];
 var GOODS_NUMBER = 26;
 var GOODS_NUMBER_BASKET = 3;
 var AMOUNT_MIN = 0;
@@ -35,19 +36,23 @@ var getRandomBoolean = function () {
 };
 
 var getRandomContents = function () {
-  var numberOfСomponents = getRandomValue(1, mokContents.length);
+  var numberOfСomponents = getRandomValue(1, MockContents.length);
   var contents = [];
+  var element;
   for (var i = 0; i < numberOfСomponents; i++) {
-    contents[i] = getRandomElement(mokContents);
+    element = getRandomElement(MockContents);
+    if (contents.indexOf(element) === -1) {
+      contents.push(element);
+    }
   }
-  return contents.toString();
+  return contents.join(', ');
 };
 
 // функция генерации объекта-товара
-var getMokElement = function () {
+var getMockElement = function () {
   var mokElement = {};
-  mokElement.name = getRandomElement(mokNames);
-  mokElement.picture = 'img/cards/' + getRandomElement(mokPictures);
+  mokElement.name = getRandomElement(mockNames);
+  mokElement.picture = 'img/cards/' + getRandomElement(mockPictures);
   mokElement.amount = getRandomValue(AMOUNT_MIN, AMOUNT_MAX);
   mokElement.price = getRandomValue(PRICE_MIN, PRICE_MAX);
   mokElement.weight = getRandomValue(WEIGHT_MIN, WEIGHT_MAX);
@@ -62,12 +67,12 @@ var getMokElement = function () {
 };
 
 // функция создания массива данных
-var getMokGoods = function (number) {
-  var mokGoods = [];
+var getMockGoods = function (number) {
+  var mockGoods = [];
   for (var i = 0; i < number; i++) {
-    mokGoods[i] = getMokElement();
+    mockGoods[i] = getMockElement();
   }
-  return mokGoods;
+  return mockGoods;
 };
 
 // функция отрисовки карточки товара по шаблону, передаем в нее данные о товаре в виде объекта и указатель на список куда отрисовывать карточку
@@ -83,16 +88,17 @@ var renderCard = function (cardData, list) {
 
   // добавим карточке товара класс доступности в зависимости от количества
   card.classList.remove('card--in-stock');
-  if (cardData.amount > 5) {
-    card.classList.add('card--in-stock');
+  switch (cardData.amount) {
+    case 0:
+      card.classList.add('card--soon');
+      break;
+    case 1: case 2: case 3: case 4:
+      card.classList.add('card--little');
+      break;
+    default:
+      card.classList.add('card--in-stock');
+      break;
   }
-  if (cardData.amount > 0 && cardData.amount <= 5) {
-    card.classList.add('card--little');
-  }
-  if (cardData.amount === 0) {
-    card.classList.add('card--soon');
-  }
-
   // название вставляем в блок card__title
   card.querySelector('.card__title').textContent = cardData.name;
 
@@ -101,7 +107,6 @@ var renderCard = function (cardData, list) {
   card.querySelector('.card__price').childNodes[2].textContent = '/ ' + cardData.weight + ' Г';
 
   // добавим нужный стиль в блок stars__rating
-  var starsRatingStyles = ['stars__rating--one', 'stars__rating--two', 'stars__rating--three', 'stars__rating--four', 'stars__rating--five'];
   card.querySelector('.stars__rating').classList.remove('stars__rating--five');
   card.querySelector('.stars__rating').classList.add(starsRatingStyles[cardData.rating.value - 1]);
 
@@ -109,22 +114,16 @@ var renderCard = function (cardData, list) {
   card.querySelector('.star__count').textContent = '( ' + cardData.rating.number + ' )';
 
   // оформим блок состав товара card__characteristic, добавим данные о наличии сахара
-  if (cardData.nutritionFacts.sugar) {
-    card.querySelector('.card__characteristic').textContent = 'Содержит сахар';
-  } else {
-    card.querySelector('.card__characteristic').textContent = 'Без сахара';
-  }
+  var sugar = cardData.nutritionFacts.sugar ? 'Содержит сахар' : 'Без сахара';
+  card.querySelector('.card__characteristic').textContent = sugar;
 
   // оформим блок состав товара card__composition-list, добавим данные о содержащихся элементах
   card.querySelector('.card__composition-list').textContent = cardData.nutritionFacts.contents;
 
-  // добавим сформированную карточку в блок товаров
+  // добавим сформированную карточку в контейнер
   list.appendChild(card);
   // catalogCards.appendChild(card);
 };
-
-// создаем массив данных товаров в каталоге
-var goods = getMokGoods(GOODS_NUMBER);
 
 // найдем блок catalog__cards и уберем у него класс catalog__cards--load
 var catalogCards = document.querySelector('.catalog__cards');
@@ -134,19 +133,22 @@ catalogCards.classList.remove('catalog__cards--load');
 var catalogLoad = document.querySelector('.catalog__load');
 catalogLoad.classList.add('visually-hidden');
 
-// отрисовываем карточки товаров в каталоге catalog__cards
+// наполним каталог: создадим данные, контейнер для отрисовки в него карточек, отрисуем в него карточки и вставим контейнер в каталог
+var goods = getMockGoods(GOODS_NUMBER);
+var fragmentCatalog = document.createDocumentFragment();
 for (var i = 0; i < goods.length; i++) {
-  renderCard(goods[i], catalogCards);
+  renderCard(goods[i], fragmentCatalog);
 }
+catalogCards.appendChild(fragmentCatalog);
 
-// создаем массив данных товаров в корзине
-var goodsInBusket = getMokGoods(GOODS_NUMBER_BASKET);
-
-// отрисуем товары в корзине в блок goods__cards
+// наполним корзину: создадим данные, контейнер, для отрисовки в него карточек, отрисуем в него карточки и вставим контейнер в корзину
+var goodsInBusket = getMockGoods(GOODS_NUMBER_BASKET);
+var fragmentBasket = document.createDocumentFragment();
 var goodsCards = document.querySelector('.goods__cards');
 for (i = 0; i < goodsInBusket.length; i++) {
-  renderCard(goodsInBusket[i], goodsCards);
+  renderCard(goodsInBusket[i], fragmentBasket);
 }
+goodsCards.appendChild(fragmentBasket);
 
 // удалим у блока товары в корзине goods__cards класс goods__cards--empty
 goodsCards.classList.remove('goods__cards--empty');
