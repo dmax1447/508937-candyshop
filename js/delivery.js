25ea6a64e1f9df6f90a79315dba915c0f58a3602
Max Dragaytsev <dmax1447@gmail.com>
2018-09-21 11:24:55 +0300

    сделал переключение табов в доставке

--- js/delivery.js
+++ js/delivery.js
@@ -2,6 +2,31 @@
 'use strict';
 (function () {
   // начало
+  var deliverySection = document.querySelector('.deliver'); // раздел доставка
+  var deliveryToggle = deliverySection.querySelector('.deliver__toggle'); // раздел выбор метода доставки
+  var tabDeliverStore = deliverySection.querySelector('.deliver__store');
+  var tabDeliverCourier = deliverySection.querySelector('.deliver__courier');
+  var btnDeliverStores = deliverySection.querySelector('#deliver__store');
+  var btnDeliverCourier = deliverySection.querySelector('#deliver__courier');
 
+  // обработчик кликов по табам выбора метода доставки
+  var onDeliveryBtnsClick = function (evt) {
+    if (evt.target === btnDeliverStores) {
+      // btnDeliverCourier.classList.add('visually-hidden');
+      // btnDeliverStores.classList.remove('visually-hidden');
+      tabDeliverStore.classList.remove('visually-hidden');
+      tabDeliverCourier.classList.add('visually-hidden');
+    }
+    if (evt.target === btnDeliverCourier) {
+      // btnDeliverCourier.classList.remove('visually-hidden');
+      // btnDeliverStores.classList.add('visually-hidden');
+      tabDeliverStore.classList.add('visually-hidden');
+      tabDeliverCourier.classList.remove('visually-hidden');
+    }
+  };
+
+
+  // добавим обработчик на раздел выбора метода доставки
+  deliveryToggle.addEventListener('click', onDeliveryBtnsClick);
   // конец
 })();
