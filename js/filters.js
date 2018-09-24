c66cf9bee2ee52ab8f8d02083ebc4f21b63cb695
Max Dragaytsev <dmax1447@gmail.com>
2018-09-24 12:44:22 +0300

    подправил баг с пинами

--- js/filters.js
+++ js/filters.js
@@ -12,16 +12,17 @@
   var rangeFillLine = catalogFilterRange.querySelector('.range__fill-line'); // полоска между пинами
   var priceMin = 100;
   var priceMax = 1500;
+  var pinSize = 10;
   // выставим начальные значения пина и бара;
   leftPin.style.left = 0;
-  rightPin.style.left = range + 'px';
-  rangeFillLine.style.left = 0;
-  rangeFillLine.style.right = 0;
+  rightPin.style.right = 0;
+  rangeFillLine.style.left = (leftPin.offsetLeft) + pinSize + 'px';
+  rangeFillLine.style.right = pinSize + 'px';
   rangePriceMin.textContent = priceMin;
   rangePriceMax.textContent = priceMax;
 
   var calculatePrice = function (x) {
-    var relativePositionInPercent = Math.round((x * 100) / range); // вычисляю положение в % от начала
+    var relativePositionInPercent = Math.round((x * 100) / (range - pinSize)); // вычисляю положение в % от начала
     return Math.round((priceMax - priceMin) * (relativePositionInPercent / 100) + priceMin); // вычисляю цену
   };
 
@@ -31,12 +32,12 @@
     var pinStart = pin.offsetLeft;
     var onPinMouseMove = function (moveEvt) {
       var pinCurrent = pinStart - (downEvt.clientX - moveEvt.clientX); // рассчитываем положение пина по сдвигу мыши и начальному положению
-      if (pin === leftPin && pinCurrent >= 0 && pinCurrent <= rightPin.offsetLeft) { // если пин левый
+      if (pin === leftPin && pinCurrent >= 0 && pinCurrent < rightPin.offsetLeft) { // если пин левый
         pin.style.left = pinCurrent + 'px';
         rangePriceMin.textContent = calculatePrice(pinCurrent);
-        rangeFillLine.style.left = pinCurrent + 'px';
+        rangeFillLine.style.left = (pinCurrent + 10) + 'px';
       }
-      if (pin === rightPin && pinCurrent >= leftPin.offsetLeft && pinCurrent < range) { // если пин правый
+      if (pin === rightPin && pinCurrent > leftPin.offsetLeft && pinCurrent <= (range - pinSize)) { // если пин правый
         pin.style.left = pinCurrent + 'px';
         rangePriceMax.textContent = calculatePrice(pinCurrent);
         rangeFillLine.style.right = (range - pinCurrent) + 'px';
