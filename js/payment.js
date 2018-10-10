e4ec59bd3ab8d624500e33442737952ec1f5a420
Max Dragaytsev <dmax1447@gmail.com>
2018-10-10 10:59:35 +0300

    убрал ошибку с полями карты, исправил сообщение валидации

--- js/payment.js
+++ js/payment.js
@@ -7,7 +7,7 @@
   var btnPaymentCash = paymentToggle.querySelector('#payment__cash'); // кнопка оплаты наличиными
   var paymentByCardTab = document.querySelector('.payment__card-wrap'); // блок оплаты картой
   var paymentByCashTab = document.querySelector('.payment__cash-wrap'); // блок оплаты наличными
-  var paymentCardInputs = paymentByCashTab.querySelectorAll('input'); // все инпуты оплаты картой
+  var paymentCardInputs = paymentByCardTab.querySelectorAll('input'); // все инпуты оплаты картой
 
   // обработчик кликов по кнопкам "оплата картой/наличными"
   var onPaymentToggleClick = function (evt) { // переключение метода оплаты по клику на кнопку
@@ -15,26 +15,26 @@
       window.payments.selectedPaymentMethod = 'card';
       paymentByCardTab.classList.remove('visually-hidden'); // таб оплата картой показываем
       paymentByCashTab.classList.add('visually-hidden'); // таб оплата налом скрываем
-      for (var i = 0; i < paymentCardInputs.length; i++) {
-        paymentCardInputs[i].removeAttribute('disabled'); // включаем инпуты оплаты картой
-      }
+      paymentCardInputs.forEach(function (input) {
+        input.removeAttribute('disabled');
+      });
     }
     if (evt.target === btnPaymentCash) { // если нал - все наооброт
       window.payments.selectedPaymentMethod = 'cash';
       paymentByCashTab.classList.remove('visually-hidden');
       paymentByCardTab.classList.add('visually-hidden');
-      for (i = 0; i < paymentCardInputs.length; i++) {
-        paymentCardInputs[i].setAttribute('disabled', '');
-      }
+      paymentCardInputs.forEach(function (input) {
+        input.setAttribute('disabled', '');
+      });
     }
   };
 
   // обработчик проверки номера карты после ввода
   cardNumberInput.addEventListener('blur', function () { // добавим валидацию карты на поле с номером карты
     if (window.payments.checkCardNumber()) {
-      document.querySelector('.payment__card-status').textContent = 'номер введен верно';
+      document.querySelector('.payment__card-status').textContent = 'Одобрен';
     } else {
-      document.querySelector('.payment__card-status').textContent = 'c карточкой проблема';
+      document.querySelector('.payment__card-status').textContent = 'Не определен';
     }
   });
 
